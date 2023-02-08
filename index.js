50e86c893b69930ae72a3a50da87680fa3167e2b
suchayann <schnyyy29@gmail.com>
2023-02-08 16:38:20 +0700

    all files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+app.all('/', (req, res) => {
+    res.send('hello!')
+})
+
+app.get('/bus', (req,res) => {
+    connection.query(
+        'select BusNumber from stationInfo where StationName = "เมเจอร์รังสิต"; ',
+        function(err, result, fields){
+            res.send(result)
+        }
+    )
+})
+
+app.listen(process.env.PORT || 3000)
