63aab8d07ce352411581d9423a777e7f008a8b96
nwps00 <nawaphorn.sut@gmail.com>
2023-02-12 16:42:58 +0700

    [Busget]:[BE]: update code BE add query

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql2')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+require("dotenv").config();
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-app.all('/', (req, res) => {
-    res.send('hello!')
-})
+app.all("/", (req, res) => {
+  res.send("hello!");
+});
 
-app.get('/bus', (req,res) => {
-    connection.query(
-        'select * from stationInfo; ',
-        function(err, result, fields){
-            res.send(result)
-        }
-    )
-})
+app.get("/bus", (req, res) => {
+  connection.query(
+    "select * from stationInfo; ",
+    function (err, result, fields) {
+      res.send(result);
+    }
+  );
+});
 
 app.get("/bus/:id", (req, res) => {
-    const searchID = req.params.id;
-    const query = 'select BusNumber from busgetApp.stationInfo where StationName like "%'+searchID+'%";';
-    connection.query(query, function (err, result, fields) {
-      res.send(result);
-    });
+  const searchID = req.params.id;
+  const query =
+    'select BusNumber from stationInfo where StationName like "%' +
+    searchID +
+    '%";';
+  connection.query(query, function (err, result, fields) {
+    res.send(result);
   });
-  
+});
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
