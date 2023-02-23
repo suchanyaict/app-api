52531f634ccf8b6ec92770f09720bd9d39dfe9b2
nwps00 <nawaphorn.sut@gmail.com>
2023-02-24 00:16:41 +0700

    update api price condition 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mysql = require("mysql2");
 require("dotenv").config();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use(cors());
 
@@ -35,43 +35,44 @@ app.get("/bus/stationName/:id", (req, res) => {
 
 app.post("/bus/price", (req, res) => {
   const { busType, distance } = req.body;
+  distance = parseFloat(distance);
   const price = 0;
-  if(busType == "red"){
+  if (busType == "red") {
     price = 8;
-  } else if(busType == "acBlue"){
-    if(distance >= 0 && distance <= 8){
+  } else if (busType == "acBlue") {
+    if (distance >= 0 && distance <= 8) {
       price = 12;
-    } else if(distance > 8 && distance <= 12 ){
+    } else if (distance > 8 && distance <= 12) {
       price = 14;
-    } else if(distance > 12 && distance <= 16 ){
+    } else if (distance > 12 && distance <= 16) {
       price = 16;
-    }else if(distance > 16 && distance <= 20 ){
+    } else if (distance > 16 && distance <= 20) {
       price = 18;
-    }else if(distance > 20 ){
+    } else if (distance > 20) {
       price = 20;
     }
-  } else if(busType == "euro2" || busType == "acPCB"){
-    if(distance >= 0 && distance <= 4){
+  } else if (busType == "euro2" || busType == "acPCB") {
+    if (distance >= 0 && distance <= 4) {
       price = 13;
-    } else if(distance > 4 && distance <= 8 ){
+    } else if (distance > 4 && distance <= 8) {
       price = 15;
-    } else if(distance > 8 && distance <= 12 ){
+    } else if (distance > 8 && distance <= 12) {
       price = 17;
-    }else if(distance > 12 && distance <= 16 ){
+    } else if (distance > 12 && distance <= 16) {
       price = 19;
-    }else if(distance > 16 && distance <= 20 ){
+    } else if (distance > 16 && distance <= 20) {
       price = 21;
-    }else if(distance > 20 && distance <= 23 ){
+    } else if (distance > 20 && distance <= 23) {
       price = 23;
-    }else if(distance > 23 ){
+    } else if (distance > 23) {
       price = 25;
     }
-  } else if(busType == "ngv"){
-    if(distance >= 0 && distance <= 4){
+  } else if (busType == "ngv") {
+    if (distance >= 0 && distance <= 4) {
       price = 15;
-    } else if(distance > 4 && distance <= 6 ){
+    } else if (distance > 4 && distance <= 6) {
       price = 20;
-    } else if(distance > 16 ){
+    } else if (distance > 16) {
       price = 25;
     }
   }
