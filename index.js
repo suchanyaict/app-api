00eed97ce84aac097cc88c75928f3adf3063fe9a
nwps00 <nawaphorn.sut@gmail.com>
2023-03-30 17:47:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -113,49 +113,47 @@ app.get("/price/:busNumber&:distance", (req, res) => {
   const query =
     'select Category from busInfo where BusNumber = "' + busNumber + '";';
   connection.query(query, function (err, result, fields) {
-    busType = result;
+    if (result === "regular") {
+      price = 8;
+    } else if (result === "AC") {
+      if (distance >= 0 && distance <= 8) {
+        price = 12;
+      } else if (distance > 8 && distance <= 12) {
+        price = 14;
+      } else if (distance > 12 && distance <= 16) {
+        price = 16;
+      } else if (distance > 16 && distance <= 20) {
+        price = 18;
+      } else if (distance > 20) {
+        price = 20;
+      }
+    } else if (result === "Euro2" || result === "acPCB") {
+      if (distance >= 0 && distance <= 4) {
+        price = 13;
+      } else if (distance > 4 && distance <= 8) {
+        price = 15;
+      } else if (distance > 8 && distance <= 12) {
+        price = 17;
+      } else if (distance > 12 && distance <= 16) {
+        price = 19;
+      } else if (distance > 16 && distance <= 20) {
+        price = 21;
+      } else if (distance > 20 && distance <= 23) {
+        price = 23;
+      } else if (distance > 23) {
+        price = 25;
+      }
+    } else if (result === "NGV") {
+      if (distance >= 0 && distance <= 4) {
+        price = 15;
+      } else if (distance > 4 && distance <= 6) {
+        price = 20;
+      } else if (distance > 16) {
+        price = 25;
+      }
+    }
+    res.send(`${price}`);
   });
-  res.send(busType);
-  // if (busType === "regular") {
-  //   price = 8;
-  // } else if (busType === "AC") {
-  //   if (distance >= 0 && distance <= 8) {
-  //     price = 12;
-  //   } else if (distance > 8 && distance <= 12) {
-  //     price = 14;
-  //   } else if (distance > 12 && distance <= 16) {
-  //     price = 16;
-  //   } else if (distance > 16 && distance <= 20) {
-  //     price = 18;
-  //   } else if (distance > 20) {
-  //     price = 20;
-  //   }
-  // } else if (busType === "Euro2" || busType === "acPCB") {
-  //   if (distance >= 0 && distance <= 4) {
-  //     price = 13;
-  //   } else if (distance > 4 && distance <= 8) {
-  //     price = 15;
-  //   } else if (distance > 8 && distance <= 12) {
-  //     price = 17;
-  //   } else if (distance > 12 && distance <= 16) {
-  //     price = 19;
-  //   } else if (distance > 16 && distance <= 20) {
-  //     price = 21;
-  //   } else if (distance > 20 && distance <= 23) {
-  //     price = 23;
-  //   } else if (distance > 23) {
-  //     price = 25;
-  //   }
-  // } else if (busType === "NGV") {
-  //   if (distance >= 0 && distance <= 4) {
-  //     price = 15;
-  //   } else if (distance > 4 && distance <= 6) {
-  //     price = 20;
-  //   } else if (distance > 16) {
-  //     price = 25;
-  //   }
-  // }
-  // res.send(`${price}`);
 });
 
 app.listen(process.env.PORT || 3000);
