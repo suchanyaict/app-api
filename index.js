905bf2b01941b8523534337aa03816fb9362b976
nwps00 <nawaphorn.sut@gmail.com>
2023-04-11 22:36:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -337,23 +337,23 @@ app.get("/busnumber/:start&:stop", (req, res) => {
           return false;
         });
 
-        finalList.forEach(function (entry) {
-          finalNum = entry.BusNumber;
-          const findTypeQuery =
-            "select Category from busInfo where BusNumber = " + finalNum + ";";
-          connection.query(findTypeQuery, function (err, resultType, fields) {
-            console.log(resultType[0].Category);
-            newObj.busType = resultType[0].Category;
-          });
-        });
-        finalList.push(newObj);
-        res.send(finalList);
+        // finalList.forEach(function (entry) {
+        //   finalNum = entry.BusNumber;
+        //   const findTypeQuery =
+        //     "select Category from busInfo where BusNumber = " + finalNum + ";";
+        //   connection.query(findTypeQuery, function (err, resultType, fields) {
+        //     console.log(resultType[0].Category);
+        //     newObj.busType = resultType[0].Category;
+        //   });
+        // });
+        // finalList.push(newObj);
+        // res.send(finalList);
 
-        // if (tempList == resultNum.length - 1) {
-        //   res.send(finalList);
-        // } else {
-        //   tempList += 1;
-        // }
+        if (tempList == resultNum.length - 1) {
+          res.send(finalList);
+        } else {
+          tempList += 1;
+        }
       });
     });
   });
